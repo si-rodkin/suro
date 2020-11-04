d15e4cf924b0608ac67649854469bbb150b29185
rodkinsi <si.rodkin@yandex.ru>
2020-11-04 17:54:18 +0300

    Поправил модель маркера (убрал nonnullable полей)

--- data_access/models/marker.py
+++ data_access/models/marker.py
@@ -5,15 +5,13 @@ from .guard_route import GuardRoute
 
 class Marker(models.Model):
     """Модель маркера"""
-    rfid = models.CharField(verbose_name="Номер RFID-карты", max_length=10, null=False, blank=False, unique=True,
-                            primary_key=True)
-    days = models.IntegerField(verbose_name="Дни обхода", null=False)
-    start_time = models.TimeField(verbose_name="Время начала обхода", null=False)
-    end_time = models.TimeField(verbose_name="Время окончания обхода", null=True)
-    time_allowance = models.IntegerField(verbose_name="Время допуска", null=False)
-    late_time = models.IntegerField(verbose_name="Время опоздания", null=False)
-    route = models.ForeignKey(GuardRoute, verbose_name="Маршрут", on_delete=models.SET_NULL, related_name="markers",
-                              null=True)
+    rfid = models.CharField(verbose_name="Номер RFID-карты", max_length=10, blank=False, unique=True, primary_key=True)
+    days = models.IntegerField(verbose_name="Дни обхода")
+    start_time = models.TimeField(verbose_name="Время начала обхода")
+    end_time = models.TimeField(verbose_name="Время окончания обхода")
+    time_allowance = models.IntegerField(verbose_name="Время допуска")
+    late_time = models.IntegerField(verbose_name="Время опоздания")
+    route = models.ForeignKey(GuardRoute, verbose_name="Маршрут", on_delete=models.SET_NULL, related_name="markers")
 
     class Meta:
         verbose_name = "Маркер"
