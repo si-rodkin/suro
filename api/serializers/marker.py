4648400480a6c25fb493df224d025640ab1af10f
rodkinsi <si.rodkin@yandex.ru>
2021-02-25 21:06:15 +0300

    Добавил вывод rfid

--- api/serializers/marker.py
+++ api/serializers/marker.py
@@ -11,4 +11,4 @@ class MarkerSerializer(ModelSerializer):
     """Сериализатор для модели маркера"""
     class Meta:
         model = Marker
-        fields = ['id', 'name', 'route']
+        fields = ['id', 'name', 'route', 'rfid']
